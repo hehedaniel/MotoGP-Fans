0d8085f29c928499809430f9e7c8f010227994d7
JoseCarlos2907 <josecarlosperezgonzalez200@gmail.com>
2024-11-26 14:21:33 +0100

    sql y ventana patrocinadores hecha

--- src/motogpfan/FXML_Datos_Piloto.java
+++ src/motogpfan/FXML_Datos_Piloto.java
@@ -3,5 +3,35 @@ package motogpfan;
 public class FXML_Datos_Piloto
 {
     @javafx.fxml.FXML
+    private ImageView ImgView_Bandera;
+    @javafx.fxml.FXML
+    private Button Btn_Volver;
+    @javafx.fxml.FXML
+    private Button Btn_MenuPrincipal;
+    @javafx.fxml.FXML
+    private Label Lbl_Peso;
+    @javafx.fxml.FXML
+    private Label Lbl_Equipo;
+    @javafx.fxml.FXML
+    private Label Lbl_Altura;
+    @javafx.fxml.FXML
+    private AnchorPane paneDatosPiloto;
+    @javafx.fxml.FXML
+    private Label Lbl_NombreCompleto;
+    @javafx.fxml.FXML
+    private Label Lbl_Numero;
+    @javafx.fxml.FXML
+    private ImageView ImgView_ImgPiloto;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleMenuPrincipalBtnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleVolverBtnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/motogpfan/FXML_Ventana_patrocinadores.java
@@ -0,0 +1,36 @@
+package motogpfan;
+
+public class FXML_Ventana_patrocinadores
+{
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador8;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador7;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador12;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador6;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador11;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador5;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador10;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador9;
+    @javafx.fxml.FXML
+    private Button Btn_MenuPrincipal;
+    @javafx.fxml.FXML
+    private AnchorPane anchorPane;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador4;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador3;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador2;
+    @javafx.fxml.FXML
+    private ImageView ImgView_Patrocinador1;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
